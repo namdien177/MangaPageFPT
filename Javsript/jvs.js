147310eaa31a58686b5cc49c2572a6c4f5238cb6
Nguyen Duc Manh <manhndgch16236@fpt.edu.vn>
2018-01-12 18:35:21 +0700

    slide and some shit

---
+++ Javsript/jvs.js
@@ -0,0 +1,24 @@
+(function(){
+  $('#updateslide').carousel({ interval: 2000 });
+  
+  $('.carousel-move .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<6;i++) {
+      
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+        console.log(itemToClone.length);
+
+    }
+  });
+}());
