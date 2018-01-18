0bf77e21a7d6633780fd1c8e2f82302329870a88
nam dohoang <34775776+OnayaKamii@users.noreply.github.com>
2018-01-18 22:47:03 +0700

    update login function but error

--- Backend-Mangahub/app.js
+++ Backend-Mangahub/app.js
@@ -6,10 +6,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var eValidatior = require('express-validator');
+
 var {User} = require('./models/user');
 var index = require('./routes/index');
 var login = require('./routes/login');
-
 let users = require('./routes/users');
 var app = express();
 // view engine setup
@@ -24,6 +25,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(eValidatior());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret:'max', saveUninitialized:false, resave:false}));
@@ -32,6 +34,7 @@ app.use(session({secret:'max', saveUninitialized:false, resave:false}));
 app.use('/', index);            //homepage
 app.use('/login', login);       //login page
 app.use('/user', users);        //profile page
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- Backend-Mangahub/routes/login.js
+++ Backend-Mangahub/routes/login.js
@@ -1,38 +1,63 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
-var app = express();
-let db = require('../DatabaseComps/DataConnection');        //      import database class
-=======
+//<<<<<<< HEAD
+//var app = express();
 //let db = require('../DatabaseComps/DataConnection');        //      import database class
->>>>>>> f35cc22f66676e9a124b6200f802bcd4ffb042f3
+//=======
+//let db = require('../DatabaseComps/DataConnection');        //      import database class
+//>>>>>>> f35cc22f66676e9a124b6200f802bcd4ffb042f3
 
 /* GET Login page */
 /**
  * Trang mặc định được gọi.
  */
 router.get('/', function(req, res, next) {
-    res.render('login');
+    res.render('login', {success: false, errors: req.session.error});
+    req.session.error = null;
 });
 
 
 /* POST Login page */
 /**
- * Login Function được đặt riêng đễ dễ dàng modify.
  * TODO: Viết lại hàm này phù hợp với mã hóa của Mạnh.
  * @param req
  * @param res
  * @param next
  */
-function loginFunct(req, res, next) {
-    
-}
+
 
 /**
  * Post for login form. Lấy function từ trên.
  * TODO: Thiết kế trang login với biến: title, loginstatus, errormessage.
  */
-router.post('/logged-in', loginFunct);
+router.post('/login', function (req, res, next) {
+    console.log('begin checking')
+    //  Check validation
+    req.check('username', 'Your username is not in form of an email!').isEmail();
+    req.check('password', 'Hey! Your password should have 6 - 20 character?.').isLength({min:6, max:20});
+
+    var allerror = req.validationErrors();
+    console.log(allerror);
+    if (allerror){
+        req.session.error = allerror;
+        console.log('Got error');
+        res.redirect('/');
+    }else {
+        //checking on serverside
+        var username = req.body.username;
+        var password = req.body.password;
+        /*var errorAuth = db.checkLogin(username, password);
+        if (errorAuth){
+            req.session.error = 'Username or Password is incorrect!';
+            res.redirect('/');
+        }
+        else {
+            res.redirect('./');
+        }*/
+        console.log('no error');
+        res.redirect('./');
+    }
+});
 
 
 module.exports = router;
