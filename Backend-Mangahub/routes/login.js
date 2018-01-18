3ffabcc0d578f5ce5359cf1613f8c6ddbf6b4e2d
nam dohoang <34775776+OnayaKamii@users.noreply.github.com>
2018-01-18 20:31:34 +0700

    prepare making login function

--- Backend-Mangahub/routes/login.js
+++ Backend-Mangahub/routes/login.js
@@ -20,28 +20,7 @@ router.get('/', function(req, res, next) {
  * @param next
  */
 function loginFunct(req, res, next) {
-    /*receive information
-    let username = req.body.username;
-    let password = req.body.password;
-    let saveuser = req.body.rememberme;
-
-    //checking on server
-    db.loginstatus(username,password, (err,rec)=>{
-        if (err){                                       //lỗi connect lên server để check password
-            req.session.username = false;
-            res.render('/', { title: 'Login', loginstatus: req.session.username, errormessage: 'There is something' +
-                'wrong with our database/server. We are sorry for this incontinent.'}); //return login page with error message
-        }else {
-            if(rec.length >0){                              //lỗi connect lên server nhưng sai tài khoản/password
-                    req.session.username = true;
-                    res.render('index', { title: 'MangaHub - Home', loginstatus: req.session.username}); //return homepage
-            }else{
-                req.session.username = false;
-                res.render('/', { title: 'Login', loginstatus: req.session.username, errormessage: 'It seems like' +
-                    'you entered wrong username/password. Please try again.'}); //return login page with error message
-            }
-        }
-    });*/
+    
 }
 
 /**
